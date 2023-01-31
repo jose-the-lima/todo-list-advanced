b12962f652cf98bb0e9ed01bca2adca4b5b6bdf0
jose-the-lima <joserennyer30@gmail.com>
2023-01-31 08:45:00 -0300

    adicionado nova fonte para o projeto, resolvido os bugs que alteravam o estado das tasks quando se adicionava um nova ou excluia ela, agora elas mantém o status atual de conclusão

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const buttonAdd = document.querySelector(".button-add-task");
 const modalCentral = document.querySelector(".central");
+const tasks = [];
+const addDayButton = document.querySelector(".button-add");
+let dataAtual;
+
+let actualTaskDay;
+
+
+
+addDayButton.addEventListener("click", () => {
+    dataAtual = new Date().toLocaleDateString("pt-br");
+
+    let sectionTitleDate = document.querySelector(".main-content section h1");
+    buttonAdd.removeAttribute("id");
+
+    sectionTitleDate.innerHTML = dataAtual;
+
+    actualTaskDay = {
+        data: dataAtual,
+        tasks: []
+    };
+});
+
 
 let tarefaASerApagada = "";
 
-
-
 const tasksArea = document.querySelector(".tasks");
-const tasks = [];
 
 const modalAdd = document.querySelector(".add-task-modal");
 
@@ -20,9 +39,16 @@ cancelButton.forEach(bt => bt.addEventListener("click", desativarModais));
 const addTaskButton = document.querySelector(".add-task");
 
 addTaskButton.addEventListener("click", () => {
+    const tasksModified = [];
+    let tasksOfMoment = document.querySelectorAll(".task");
+
+    tasksOfMoment.forEach((tas) => {
+        tasksModified.push([tas.children[1].innerHTML, tas.getAttribute("id") != null ? true:false]);
+    });
+
     const task = document.querySelector(".campo-add");
 
-    tasks.push(task.value);
+    actualTaskDay.tasks = [...tasksModified, [task.value, false]];
 
     task.value = "";
     
@@ -37,7 +63,19 @@ const deleteTaskButton = document.querySelector(".delete-task");
 
 
 deleteTaskButton.addEventListener("click", () => {
-    tasks.splice(tasks.indexOf(tarefaASerApagada), 1);
+
+    const tasksCurrent = document.querySelectorAll(".task"); 
+
+    let tasksRemaining = [];
+
+    tasksCurrent.forEach((task) => {
+        const contentOfTask = task.children[1].innerHTML;
+        const taskCompleteStatus = task.getAttribute("id") != null ? true : false;
+
+        contentOfTask != tarefaASerApagada ? tasksRemaining.push([contentOfTask, taskCompleteStatus]):null;
+    });
+
+    actualTaskDay.tasks = tasksRemaining;
 
     renderizarListaDeTarefas();
 
@@ -59,15 +97,19 @@ function renderizarListaDeTarefas() {
 
     tasksArea.innerHTML = "";
 
-    tasks.forEach((task) => {
-        tasksArea.innerHTML += `<div class="task">
-        <input type="checkbox" class="check-task"/>
-        <p>${task}</p>
+    actualTaskDay.tasks.forEach((task) => {
+        tasksArea.innerHTML += taskModel(task);
+    });
+}
+
+function taskModel(task) {
+    return `<div class="task" ${task[1] ? `id="checked"`:``}>
+        <input type="checkbox" class="check-task" ${task[1] ? "checked":""}/>
+        <p>${task[0]}</p>
         <button class="bt-trash">
             <img src="./assets/trash.png"/>
         </button>
     </div>`
-    });
 }
 
 
